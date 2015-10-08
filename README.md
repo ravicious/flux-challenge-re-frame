ce6906cc75f75b7e79352eaf4732d320e91ae5ad
Rafał Cieślak <ravicious@gmail.com>
2015-10-08 18:54:27 +0200

    lein new figwheel

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # flux-challenge-re-frame
-Flux Challenge done in re-frame
+
+This is a [flux-challenge](https://github.com/staltz/flux-challenge) submission written in
+[ClojureScript](https://github.com/clojure/clojurescript) using
+[re-frame](https://github.com/Day8/re-frame) pattern.
+
+## Setup
+
+To get an interactive development environment run:
+
+    lein figwheel
+
+and open your browser at [localhost:3449](http://localhost:3449/). This will auto compile and send
+all changes to the browser without the need to reload. After the compilation process is complete,
+you will get a Browser Connected REPL. An easy way to try it is:
+
+    (js/alert "Am I connected?")
+
+and you should see an alert in the browser window.
+
+To clean all compiled files:
+
+    lein clean
+
+To create a production build run:
+
+    lein cljsbuild once min
+
+And open your browser in `resources/public/index.html`. You will not get live reloading, nor a REPL.
---
+++ project.clj
@@ -0,0 +1,66 @@
+(defproject flux-challenge-re-frame "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [reagent "0.5.1"]
+                 [re-frame "0.4.1"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.1"]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+
+                        :figwheel { :on-jsload "flux-challenge-re-frame.core/on-js-reload" }
+
+                        :compiler {:main flux-challenge-re-frame.core
+                                   :asset-path "js/compiled/out"
+                                   :output-to "resources/public/js/compiled/flux_challenge_re_frame.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "min"
+                        :source-paths ["src"]
+                        :compiler {:output-to "resources/public/js/compiled/flux_challenge_re_frame.js"
+                                   :main flux-challenge-re-frame.core
+                                   :optimizations :advanced
+                                   :pretty-print false}}]}
+
+  :figwheel {
+             ;; :http-server-root "public" ;; default and assumes "resources" 
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1" 
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+             ;; doesn't work for you just run your own server :)
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
+             })
